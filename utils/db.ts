0ab295b0be60e36470e80e268f923a695b1cbbe9
Theeratdolchat Chatchai <sitthach7777@gmail.com>
2025-10-23 23:58:07 +0700

    change sql pool

--- utils/db.ts
+++ utils/db.ts
@@ -1,8 +1,31 @@
-import mysql from 'mysql2/promise';
-if (!process.env.MYSQL_URI) {
-  throw new Error('MYSQL_URI is not defined in environment variables');
+// utils/db.ts
+import mysql, { Pool } from 'mysql2/promise';
+
+let cachedPool: Pool;
+
+export function getMysqlPool(): Pool {
+  if (!process.env.MYSQL_URI) {
+    throw new Error('MYSQL_URI is not defined in environment variables');
+  }
+
+  if (cachedPool) {
+    return cachedPool;
+  }
+
+  if (process.env.NODE_ENV === 'development') {
+    const globalWithPool = global as typeof global & {
+      mysqlPool: Pool;
+    };
+
+    if (!globalWithPool.mysqlPool) {
+      globalWithPool.mysqlPool = mysql.createPool({ uri: process.env.MYSQL_URI });
+    }
+    cachedPool = globalWithPool.mysqlPool;
+  } else {
+    cachedPool = mysql.createPool({ uri: process.env.MYSQL_URI });
+  }
+
+  return cachedPool;
 }
 
-export const mysqlPool = mysql.createPool({
-  uri: process.env.MYSQL_URI,
-});
+export const mysqlPool = getMysqlPool();
